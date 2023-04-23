273cb81066b7ecca5dc7f1b0567368a3d258dba0
Frantz Paul <fpaul787@gmail.com>
2023-04-23 17:44:38 -0400

    making auth user model work and fixing typo

--- BaseRoot/urls.py
+++ BaseRoot/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api", include(("core.routers", "core"), namespace="core-api"))
+    path("api/", include(("core.routers", "core"), namespace="core-api"))
 ]
