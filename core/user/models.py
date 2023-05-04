f27c4894553e4447fb554c78192c38dfb2be7dac
Frantz Paul <fpaul787@gmail.com>
2023-05-03 23:54:24 -0400

    Adding abstract items for DRY

--- core/user/models.py
+++ core/user/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
 )
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
+from core.abstract.models import AbstractModel, AbstractManager
 
 
 class UserManager(BaseUserManager):
@@ -50,20 +51,13 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(
-        db_index=True,
-        unique=True,
-        default=uuid.uuid4,
-        editable=False)
+class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     objects = UserManager()
