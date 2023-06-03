12bc218826a89df1f3c129305a4d2d872f889188
Frantz Paul <fpaul787@gmail.com>
2023-06-03 17:57:45 -0400

    Protected routes

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from './routes/ProtectedRoute';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      <Route path="/login/" element={<div>Login</div>}></Route>
     </Routes>
   );
 }
